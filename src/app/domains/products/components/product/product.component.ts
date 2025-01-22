428b7da7e116774e1bece66b49fc2449b44ed8b8
Fabian2023 <fasalpe34@gmail.com>
2025-01-22 14:07:23 -0500

    feat(exudus commerce): :rocket:

---
+++ src/app/domains/products/components/product/product.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css',
+})
+export class ProductComponent {
+  @Input({required:true}) img: string = '';
+  @Input() title: string = '';
+  @Input() price: number = 0;
+}
